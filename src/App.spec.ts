02ac29eaa309010f3a718376131b5b2b76e25386
dhe <keke@dhes-mbp.lan>
2021-02-18 23:29:22 -0800

    fix typescript support

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,10 +1,17 @@
 import { render } from "@testing-library/svelte";
 import App from "./App.svelte";
 
-test(App.name, () => {
+test('test 1', () => {
   const _name: string = 'world';
 
   const results = render(App, { props: { name: _name } });
 
   expect(() => results.getByText("Hello world!")).not.toThrow();
+});
+
+test('test 2', () => {
+
+const {getByTestId} = render(App, {name: 'test'});
+
+expect(getByTestId('name')).toBeInTheDocument();
 });
