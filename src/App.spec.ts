df6422a6a31d57e03ac58fe44f9123b451cedde2
dhe <keke@dhes-mbp.lan>
2021-02-19 00:33:20 -0800

    add eslint

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -1,12 +1,13 @@
 import { render } from "@testing-library/svelte";
 import App from "./App.svelte";
 
+
 test('test 1', () => {
   const _name: string = 'world';
 
   const results = render(App, { props: { name: _name } });
 
-  expect(() => results.getByText("Hello world!")).not.toThrow();
+  expect(() => results.getByText('Hello world!')).not.toThrow();
 });
 
 test('test 2', () => {
